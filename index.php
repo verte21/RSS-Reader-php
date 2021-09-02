427ba8f2142abc65bdf2aad691d90e991bd28e9a
Verte21 <[verte21rs@gmail.com]>
2021-09-02 22:17:29 +0200

    favicon added

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RSS-Reader</title>
     <link rel="stylesheet" href="css/bootstrap.css">
-
+    <link rel="shortcut icon" type="image/jpg" href="rss-icon.ico"/>
 </head>
 
 
